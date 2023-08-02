5af38acbab00863c0671324ff22fb6cf9171e10e
rekha912 <rekhamisal91294@gmail.com>
2023-08-02 16:40:54 +0530

    initial commit

---
+++ AllFeatureFiles/login.feature
@@ -0,0 +1,5 @@
+Feature: Test the Orange HRM Application
+Scenario: Test Login Functionality
+Given User open a orange HRM URL
+When User enter valid UserName and valid password
+Then User on home page and valid home page title
---
+++ src/test/java/StepDefination/LoginPageStepDef.java
@@ -0,0 +1,63 @@
+package StepDefination;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import junit.framework.Assert;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import junit.framework.Assert;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginPageStepDef {
+
+	WebDriver driver;
+
+	@Given("User open a orange HRM URL")
+	public void user_open_a_orange_hrm_url() {
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+	    
+	}
+	@When("User enter valid UserName and valid password")
+	public void user_enter_valid_user_name_and_valid_password() throws Exception {
+		Thread.sleep(5000);
+		driver.findElement(By.name("username")).sendKeys("Admin");
+		driver.findElement(By.name("password")).sendKeys("admin123");
+		driver.findElement(By.xpath("//button[text()=' Login ']")).click(); 
+	}
+	@Then("User on home page and valid home page title")
+	public void user_on_home_page_and_valid_home_page_title() {
+		String actualTitle = driver.getTitle();
+
+		Assert.assertEquals(actualTitle, "OrangeHRM"); 
+	}
+
+
+	
+}
